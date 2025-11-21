acdba9f883fa2ecca94920b7ebc02b94c9dee8cf
Ravi <rvsharma008@gmail.com>
2025-11-21 15:28:34 +0530

    2111-1

--- Techugo.POS.ECOm/App.xaml.cs
+++ Techugo.POS.ECOm/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using MaterialDesignThemes.Wpf;
+using MaterialDesignThemes.Wpf;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MaterialDesignThemes.Wpf;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media;
 using Techugo.POS.ECom.Model;
 
@@ -39,6 +41,15 @@ namespace Techugo.POS.ECOm
             services.Configure<ApiSettings>(configuration.GetSection("ApiSettings"));
 
             ServiceProvider = services.BuildServiceProvider();
+
+            var appFont = new FontFamily("Segoe UI, 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji', 'Apple Color Emoji'");
+
+            // Make Control/TextBlock/TextElement default to the specified font family
+            // C#
+            var style = new Style(typeof(Control));
+            style.Setters.Add(new Setter(Control.FontFamilyProperty, appFont));
+            Application.Current.Resources[typeof(Control)] = style;
+
         }
     }
 }
