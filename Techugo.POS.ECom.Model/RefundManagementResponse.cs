2dd17e9a5270c3bc860d0c4de0ebd0024d4aaecc
Ravi <rvsharma008@gmail.com>
2026-04-28 18:45:43 +0530

    refund

---
+++ Techugo.POS.ECom.Model/RefundManagementResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Techugo.POS.ECom.Model
+{
+    public class RefundManagementResponse : BaseResponse
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("totalItems")]
+        public int TotalItems { get; set; }
+
+        [JsonPropertyName("totalPages")]
+        public int TotalPages { get; set; }
+        [JsonPropertyName("data")]
+        public List<RefundData> Data { get; set; }
+    }
+    public class RefundData
+    {
+        public decimal RefundAmount { get; set; }
+
+        [JsonPropertyName("createdAt")]
+        public DateTime CreatedAt { get; set; }
+        public string OrderNo { get; set; }
+        public string OrderID { get; set; }
+        public Customer Customer { get; set; }
+        public OrderAddress OrderAddress { get; set; }
+
+        public object OrderDetails { get; set; }
+    }
+}
